204fe9d37abf5f25266164c89ffe13016bbe6d74
Sachin Sabat <sabat.sachin33@gmail.com>
2021-03-04 16:50:28 +0530

    SearchVC added

---
+++ VividPhotography/Modules/Gallery/VIews/SearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  SearchVC.swift
+//  VividPhotography
+//
+//  Created by Sachin Sabat on 03/03/21.
+//
+
+import UIKit
+
+final class SearchVC: UIViewController {
+    
+    override func viewDidLoad() {
+        print("Search Vc")
+
+        super.viewDidLoad()
+        view.backgroundColor = .appBackground()
+    }
+
+}
+
+
